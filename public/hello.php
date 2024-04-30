f1a80d9a9b7ed2fb51bba1fdee47d0b8a39e1184
Seki Takumi <kd1326575@st.kobedenshi.ac.jp>
2024-04-30 13:48:06 +0900

    提出

--- public/hello.php
+++ public/hello.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<!-- 今回は初回なので、仮のファイルを用意しましたが、今後は指示に従い、
-    自分でファイルを作っていって下さい -->
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- この領域(bodyタグ内)に処理内容を入れてください -->
-</body>
-</html>
+<!DOCTYPE html> <!-- ① -->
+    <!-- 今回は初回なので、仮のファイルを用意しましたが、今後は指示に従い、自分でファイルを作っていって下さい -->
+    <html lang="ja"> <!-- ② -->
+    <head>
+        <meta charset="UTF-8"> <!-- ③ -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <!-- この領域(bodyタグ内)に処理内容を入れてください -->
+      <?php // ④
+        echo '<p>Hello,PHP</p>'; // ⑤
+        ?>　<!-- ⑥ -->
+        <p>0J04015関拓海</p>
+        <!-- 今回はここまで -->
+    </body>
+    </html>
